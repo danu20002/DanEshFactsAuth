4d88e690df13a71166762a3f88a61acdf18ad9cc
Danesh Naik <naikdanesh2@gmail.com>
2024-06-29 04:31:22 +0530

    initial commit

---
+++ azurefactsauth/src/main/java/com/danesh/azurefactsauth/AzurefactsauthApplication.java
@@ -0,0 +1,13 @@
+package com.danesh.azurefactsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurefactsauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurefactsauthApplication.class, args);
+	}
+
+}
---
+++ azurefactsauth/src/test/java/com/danesh/azurefactsauth/AzurefactsauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danesh.azurefactsauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurefactsauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
